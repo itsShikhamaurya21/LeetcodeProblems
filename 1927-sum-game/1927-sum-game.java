6f5fc9393998d0ddf245ae1e062669215f9f4cb8
itsShikhamaurya21 <shikhamaurya987373@gmail.com>
2026-08-23 14:40:39 +0530

    Time: 11 ms (5.11%), Space: 46.6 MB (97.81%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean sumGame(String num) {
+       int leftsum=0;
+       int rightsum=0;
+       int leftQ=0;
+       int rightQ=0;
+       int n=num.length();
+       for(int i=0;i<n/2;i++){
+        if(num.charAt(i)=='?'){
+            leftQ++;
+        }else{
+            leftsum=leftsum+(num.charAt(i)-'0');
+        }
+        if(num.charAt(n-1-i)=='?'){
+            rightQ++;
+        }else{
+            rightsum=rightsum+(num.charAt(n-1-i)-'0');
+        }
+       } 
+       int diff=leftsum-rightsum;
+       int diffq=leftQ-rightQ;
+       return Math.abs(diffq) % 2 == 1
+       || diff != -9 * diffq / 2;
+    }
+}
