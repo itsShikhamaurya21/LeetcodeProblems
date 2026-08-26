0f36b55df9139fc37f9dc329e07900980e9269d1
itsShikhamaurya21 <shikhamaurya987373@gmail.com>
2026-08-26 22:25:55 +0530

    Time: 0 ms (100%), Space: 42.3 MB (79.51%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,14 +1,23 @@
 class Solution {
     public boolean isHappy(int n) {
-        HashSet<Integer> set=new HashSet<>();
-        while(n!=1){
-            if(set.contains(n)){
-                return false;
-            }
-            set.add(n);
-            n=sumofsquares(n);
+        // HashSet<Integer> set=new HashSet<>();
+        // while(n!=1){
+        //     if(set.contains(n)){
+        //         return false;
+        //     }
+        //     set.add(n);
+        //     n=sumofsquares(n);
+        // }
+        // return true;  //TC=O(logn) SC=O(n)
+
+        // Floyd detection algorithm
+        int slow=n,fast=n;
+        while(true){
+            slow=sumofsquares(slow);
+            fast=sumofsquares(sumofsquares(fast));
+            if(fast==1) return true;
+            if(slow==fast) return false;
         }
-        return true;
     }
     public static int sumofsquares(int n){
         int sum=0;
