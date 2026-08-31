5e82864007ff5acadb0e9e9a248a8cec9bb8dc73
itsShikhamaurya21 <shikhamaurya987373@gmail.com>
2026-08-31 21:36:27 +0530

    Time: 28 ms (5.13%), Space: 46.7 MB (9.05%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,14 +1,32 @@
 class Solution {
     public static boolean isAnagram(String str1, String str2) {
         if(str1.length()!=str2.length()) return false;
-        char[] arr1=str1.toCharArray();
-        char[] arr2=str2.toCharArray();
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        for(int i=0;i<arr1.length;i++){
-            if(arr1[i]!=arr2[i]) return false;
+        // char[] arr1=str1.toCharArray();
+        // char[] arr2=str2.toCharArray();
+        // Arrays.sort(arr1);
+        // Arrays.sort(arr2);
+        // for(int i=0;i<arr1.length;i++){
+        //     if(arr1[i]!=arr2[i]) return false;
+        // }
+        // return true;
+        HashMap<Character,Integer> map=new HashMap<>();
+
+        for(int i=0;i<str1.length();i++){
+            char ch=str1.charAt(i);
+            if(map.containsKey(ch)){
+                int freq=map.get(ch);
+                map.put(ch,freq+1);
+            }else{
+                map.put(ch,1);
+            }
         }
-        return true;
+        for(int i=0;i<str2.length();i++){
+            char ch=str2.charAt(i);
+            if(!map.containsKey(ch)) return false;
+            map.put(ch,map.get(ch)-1);
+            if(map.get(ch)==0) map.remove(ch); //cleanup
+        }
+        return map.isEmpty();
     }
 
 }
