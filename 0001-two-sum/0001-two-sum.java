3769202ea586666a8b63677564e852ba0fa64091
itsShikhamaurya21 <shikhamaurya987373@gmail.com>
2026-09-10 23:28:36 +0530

    Time: 2 ms (99.38%), Space: 47.2 MB (23.84%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -10,14 +10,6 @@ class Solution{
         // return new int[]{-1,-1};
 
         HashMap<Integer,Integer> map=new HashMap<>();
-        // for(int i=0;i<arr.length;i++){
-        //     if(map.containsKey(arr[i])){
-        //         int freq=map.get(arr[i]);
-        //         map.put(arr[i],freq+1);
-        //     }else{
-        //         map.put(arr[i],1);
-        //     }
-        // }
         for(int i=0;i<arr.length;i++){
             int tar=target-arr[i];
             if(map.containsKey(tar)){
