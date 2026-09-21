ba21b3395e02c8a5ff6eef245dbcdeadbc1ed3c7
itsShikhamaurya21 <shikhamaurya987373@gmail.com>
2026-09-21 21:13:18 +0530

    Time: 34 ms (57.4%), Space: 59.3 MB (45.39%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,51 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        // List<List<Integer>> ans = new ArrayList<>();
+        // Arrays.sort(nums);
+        // for(int i=0;i<nums.length-2;i++){
+        //     if(i>0&& nums[i]==nums[i-1]) continue;
+        //     int j=i+1;
+        //     int k=nums.length-1;
+        //     while(j<k){
+        //         if(nums[i]+nums[j]+nums[k]<0) j++;
+        //         else if (nums[i]+nums[j]+nums[k]>0) k--;
+        //         else{
+        //             ans.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                
+        //         while(j<k && nums[j]==nums[j+1]) j++;
+        //         while(j<k && nums[k]==nums[k-1]) k--;
+                
+        //         j++;
+        //         k--;
+        //         }
+        //     }
+        // }
+        //     return ans;
+        // }
+    
+    List<List<Integer>> ans=new ArrayList<>();
+    Arrays.sort(nums);
+    int sum=0;
+    for(int i=0;i<nums.length-2;i++){
+        if(i>0 && nums[i]==nums[i-1]) continue;
+        int left=i+1;
+        int right=nums.length-1;
+        sum=-1*nums[i];
+        while(left<right){
+            int s=nums[left]+nums[right];
+            if(s==sum){
+                ans.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                left++;
+                right--;
+                while(left<nums.length && nums[left]==nums[left-1]) left++;
+                while(right>=0 && nums[right]==nums[right+1]) right--;
+            }else if(s<sum){
+                left++;
+            }else{
+                right--;
+            }
+        }
+    }
+            return ans;
+}
+}
