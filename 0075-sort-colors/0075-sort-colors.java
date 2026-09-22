2245da03821005762e7868b8da44cf63dc2df348
itsShikhamaurya21 <shikhamaurya987373@gmail.com>
2026-09-22 14:42:40 +0530

    Time: 0 ms (100%), Space: 43.5 MB (39.82%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,22 +1,42 @@
 class Solution {
     public void sortColors(int[] nums) {
         
-        int mid=0,high=nums.length-1,low=0;
-        while(mid<=high){
-            if(nums[mid]==0){
-                int temp=nums[mid];
-                nums[mid]=nums[low];
-                nums[low]=temp;
-                mid++;
-                low++;
-            }else if(nums[mid]==1){
-                mid++;
-            }else{
-                int temp=nums[mid];
-                nums[mid]=nums[high];
-                nums[high]=temp;
-                high--;
-            }
+        // int mid=0,high=nums.length-1,low=0;
+        // while(mid<=high){
+        //     if(nums[mid]==0){
+        //         int temp=nums[mid];
+        //         nums[mid]=nums[low];
+        //         nums[low]=temp;
+        //         mid++;
+        //         low++;
+        //     }else if(nums[mid]==1){
+        //         mid++;
+        //     }else{
+        //         int temp=nums[mid];
+        //         nums[mid]=nums[high];
+        //         nums[high]=temp;
+        //         high--;
+        //     }
+        // }
+
+        int zero=0;
+        int ones=0;
+        int two=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0) zero++;
+            else if(nums[i]==1) ones++;
+            else two++;
         }
-    }
+        int k=0;
+       for(int i=0;i<zero;i++){
+            nums[k++]=0;
+        }
+        for(int i=0;i<ones;i++){
+            nums[k++]=1;
+        }
+        for(int i=0;i<two;i++){
+            nums[k++]=2;
+        }
+
+            }
 }
